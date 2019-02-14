eafe52dd7cbd20515f4b01c00fce524c72d6dd0c
G_Edward06 <chuncheng.gu@gmail.com>
2019-02-14 18:58:06 +0900

    修改calendar.prefab，补充代码

--- Assets/Scripts/calendar.cs
+++ Assets/Scripts/calendar.cs
@@ -2,38 +2,106 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class calendar : MonoBehaviour
 {
     [SerializeField] Button today;
+    [SerializeField] Button previous;
+    [SerializeField] Button next;
+ 
+    private const int numOfbuttons = 37;
 
-    private Button[] days;
-    private int numOfdays = 31; // Number of building tier buttons;
+    private Button[] buttons;
+    private int month;
+    private int year;
+    private int numOfdays;
+    private DateTime dateValue;
+    private int dayOfweek;
 
     // ==============================================================
     void Awake()
     {
-        days = new Button[numOfdays];
-        int index = 1;
+        buttons = new Button[numOfbuttons];
 
+        buttonClear();
+        year = System.DateTime.Today.Year;
+        month = System.DateTime.Today.Month;
+        SetDate();
+
+        today.GetComponentInChildren<Text>().text = System.DateTime.Today.ToString("yyyy-MM-dd");
+    }
+
+    private void SetDate()
+    {
+        numOfdays = System.DateTime.DaysInMonth(year, month);
+        dateValue = new DateTime(year, month, 1);
+        dayOfweek = (int)dateValue.DayOfWeek;
+
+        int index = 1;
         for (int i = 0; i < numOfdays; i++)
         {
-            Button _days = GameObject.Find("button_" + index).GetComponent<Button>();
-            days[i] = _days;
-            days[i].GetComponentInChildren<Text>().text = index.ToString();
+            buttons[dayOfweek + i].GetComponentInChildren<Text>().text = index.ToString();
             index++;
+        }
+    }
+
+    private void buttonClear()
+    {
+        for (int i = 0; i < numOfbuttons; i++)
+        {
+            Button _buttons = GameObject.Find("Button_" + i).GetComponent<Button>();
+            buttons[i] = _buttons;
+            buttons[i].GetComponentInChildren<Text>().text = "";
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        today.GetComponentInChildren<Text>().text = System.DateTime.Today.ToLongDateString();
+        today.onClick.AddListener(todayOnClick);
+        previous.onClick.AddListener(preOnClick);
+        next.onClick.AddListener(nextOnClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void nextOnClick()
     {
-        
+        buttonClear();
+        if (month == 12)
+        {
+            year += 1;
+            month = 1;
+        }
+        else
+        {
+            month += 1;
+        }
+        SetDate();
+        today.GetComponentInChildren<Text>().text = dateValue.ToString("yyyy-MM");
+    }
+
+    private void preOnClick()
+    {
+        buttonClear();
+        if(month == 1)
+        {
+            year -= 1;
+            month = 12;
+        }
+        else
+        {
+            month -= 1;
+        }      
+        SetDate();
+        today.GetComponentInChildren<Text>().text = dateValue.ToString("yyyy-MM");
+    }
+
+    private void todayOnClick()
+    {
+        buttonClear();
+        year = System.DateTime.Today.Year;
+        month = System.DateTime.Today.Month;
+        SetDate();
+        today.GetComponentInChildren<Text>().text = System.DateTime.Today.ToString("yyyy-MM-dd");
     }
 }
